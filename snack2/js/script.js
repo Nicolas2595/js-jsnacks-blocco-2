82cba633bd10d43e9686ba7a141c5bada025c8bb
Nicolas2595 <nmorelli10@gmail.com>
2021-05-11 14:50:06 +0200

    snack2

---
+++ snack2/js/script.js
@@ -0,0 +1,69 @@
+/*
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate inizialmente a 0.
+Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+Infine creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+*/
+
+// FUNZIONI
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+// FINE FUNZIONI
+
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate inizialmente a 0.
+
+var arraySquadre = [
+
+    {
+        nome: "Bologna",
+        puntiFatti:0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Milan",
+        puntiFatti: 0, 
+        falliSubiti: 0
+    },
+    {
+        nome: "Fiorentina",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Verona",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        nome: "Roma",
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+
+];
+
+console.log(arraySquadre);
+
+// Generare numeri random al posto degli 0 nelle proprietà: punti fatti e falli subiti.
+
+var arraySquadreNuovo = [];
+
+for(var i = 0; i < arraySquadre.length; i++) {
+
+    arraySquadre[i].puntiFatti = getRandomNumber(0, 100);
+    arraySquadre[i].falliSubiti = getRandomNumber(0, 100);
+
+    // Infine creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+    var statistiche = {};
+        
+        statistiche.nome = arraySquadre[i].nome;
+        statistiche.falliSubiti = arraySquadre[i].falliSubiti;
+
+        arraySquadreNuovo.push(statistiche);
+
+}
+
+console.log(arraySquadreNuovo);
